8ddd7d283a63df3070579990f1afc8a53076bf7c
BT_3W2_11 <raymondg89@nycstudents.net>
2022-10-13 09:19:41 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        String blah = "What's for dinner?";
+        String ugh = blah.substring(11,17);
+        System.out.println(ugh);
+
+    }
+}
